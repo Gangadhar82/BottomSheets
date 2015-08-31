1247ea16d69b5559cebb743475af8fe8fd8da200
Gangadhar82 <gangadhar.g@mmadapps.com>
2015-08-31 16:14:11 +0530

    in mainactivity

--- app/src/main/java/com/mmadapps/bottomsheets/MainActivity.java
+++ app/src/main/java/com/mmadapps/bottomsheets/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.customBottomSheet).setOnClickListener(this);
         findViewById(R.id.customGridBottomSheet).setOnClickListener(this);
         //fkjakljtiewjtioeuqwijtgkadjsgklajdiojyiraekjtogkajga
+        Log.d("gnaga","hiiii");
     }
 
     @Override
